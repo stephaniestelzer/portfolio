1d0181b16fdcd1b6ac644c42250c353e78a85755
Stephanie Stelzer <stephanie@stelzers.com>
2025-08-07 21:19:30 -0400

    changing to a four column layout

--- app/components/ProjectFilter.tsx
+++ app/components/ProjectFilter.tsx
@@ -79,7 +79,7 @@ export default function ProjectFilter({ projects }: ProjectFilterProps) {
 
       {/* Projects Grid */}
       {filteredProjects.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {filteredProjects.map((project) => (
             <ProjectCard key={project._id} project={project} />
           ))}
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -42,7 +42,7 @@ export default async function ProjectsPage() {
     <>
       <Header />
       <Layout>
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-16 py-16">
           <ProjectFilter projects={projects} />
         </div>
       </Layout>
