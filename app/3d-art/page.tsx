523a89c2fbd9f7d77162b768b2fc0f3cc58d4448
Stephanie Stelzer <stephanie@stelzers.com>
2025-08-15 07:56:28 -0400

    3D art page setup

--- app/3d-art/page.tsx
+++ app/3d-art/page.tsx
@@ -1,9 +1,22 @@
+import { Layout } from '../components';
+import { Header } from '../components';
+
 export default async function Art() {
     return (
         <>
-            <div>
-                <p>hello world!</p>
-            </div>
-        </>
+      <Header />
+      <Layout>
+        <div className="min-h-screen pt-8">
+          <div className="flex mt-8 mb-8 flex-col justify-center lg:flex-row">
+            In the process of moving this work from a previous portfolio. Coming soon.
+          </div>
+        </div>
+      </Layout>
+      <div className="py-8 text-center bg-tag-unselected">
+        <p className="text-grey-400 font-light text-p">
+          © 2025 Stephanie Stelzer
+        </p>
+      </div>
+    </>
     )
 }
--- app/notebook/page.tsx
+++ app/notebook/page.tsx
@@ -1,6 +1,5 @@
 import { Layout } from '../components';
 import { Header } from '../components';
-import Image from 'next/image';
 
 export default async function Notebook() {
 
